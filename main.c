aea944958977e1e4f2dff0738da8d3c90ff4e3a7
Night-Voyager <18722007@bjtu.edu.cn>
2021-12-01 19:47:49 +0800

    feat: recognize keywords

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+
+static char keywords[][11] = {
+    "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float",
+    "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch",
+    "typeof", "union", "unsigned", "void", "volatile", "while",  // 32 keywords for ANSI C
+    "_Bool", "_Complex", "_Imaginary", "inline", "restrict"  // 5 new keywords for C99
+};
+
+int isKeyword(char *);
 
 int main() {
-    printf("Hello, World!\n");
+    printf("%d\n", isKeyword("char"));
     return 0;
+}
+
+int isKeyword(char * s) {
+    int i;
+    for (i = 0; i < 37; ++i) {
+        if (strcmp(s, keywords[i]) == 0) break;
+    }
+    return strcmp(s, keywords[i]);
 }
