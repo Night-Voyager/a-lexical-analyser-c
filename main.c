ee796352c81bc3aa752fcea0ef2caf3f7b0addc5
Night-Voyager <18722007@bjtu.edu.cn>
2021-12-02 20:08:51 +0800

    feat: handle constants
    including single characters, strings, and numbers

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+#define IDENTIFIER_MAX_LEN 32
+
 static char keywords[][11] = {
     "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float",
     "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch",
@@ -10,13 +12,14 @@ static char keywords[][11] = {
     "_Bool", "_Complex", "_Imaginary", "inline", "restrict"  // 5 new keywords for C99
 };
 
+char token[100] = {'\0'};
+int token_length = 0;
+
 int isKeyword(char *);
 void handleComments(FILE *);
+void handleConstants(FILE *, char);
 
 int main() {
-    char token[32] = {'\0'};
-    int token_length = 0;
-
     FILE * file = fopen("../main.c", "r");
     char c;
 
@@ -26,6 +29,12 @@ int main() {
             // handle comments
             if (c == '/' && token_length == 0) {
                 handleComments(file);
+                continue;
+            }
+
+            // handle constants
+            if ( (isdigit(c) || c == '\'' || c == '\"') && token_length == 0 ) {
+                handleConstants(file, c);
                 continue;
             }
 
@@ -68,5 +77,73 @@ void handleComments(FILE * file) {
         default:
             printf("Error: invalid token");
             exit(0);
+    }
+}
+
+void handleConstants(FILE * file, char c) {
+    switch (c) {
+        case '\'':
+            // handle single character
+            token[token_length++] = c;
+
+            c = getc(file);
+            if (c == '\\') {
+                token[token_length++] = c;
+                c = getc(file);
+            }
+            token[token_length++] = c;
+
+            if ( (c = getc(file)) != '\'' ) {  // handle error
+                printf("warning: multi-character character constant");
+                exit(0);
+            }
+
+            token[token_length++] = c;
+            token[token_length++] = '\0';
+            token_length = 0;
+
+            printf("char: %s\n", token);
+
+            break;
+        case '\"':
+            // handle string
+            do {
+                token[token_length++] = c;
+                if (c == '\\')
+                    token[token_length++] = getc(file);
+            } while ( (c = getc(file)) != '\"' && c != '\r' && c != '\n');
+
+            if (c == '\r' || c == '\n') {  // handle error
+                printf("error: missing terminating \" character");
+                exit(0);
+            }
+
+            token[token_length++] = c;
+            token[token_length] = '\0';
+            token_length = 0;
+
+            printf("string: %s\n", token);
+
+            break;
+        default:
+            // handle number
+            while (isdigit(c)) {
+                token[token_length++] = c;
+                c = getc(file);
+            }
+
+            if (c == '.') {
+                token[token_length++] = c;
+                c = getc(file);
+                while (isdigit(c)) {
+                    token[token_length++] = c;
+                    c = getc(file);
+                }
+            }
+
+            token[token_length] = '\0';
+            token_length = 0;
+
+            printf("num: %s\n", token);
     }
 }
